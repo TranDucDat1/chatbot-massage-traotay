3ab601ca46198496972522b0c433f400b2ca8796
Duc Dat <datnho53@gmail.com>
2023-07-11 19:25:40 +0700

    fix again

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -23,23 +23,43 @@ let getWebhook = (req, res) => {
   }
 };
 
-let postWebhook = (req, res) => {
+
+let postWebhook = (req, res) =>{
+  // Parse the request body from the POST
   let body = req.body;
 
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-    body.entry.forEach(function(entry) {
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
 
-      //get sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-    });
-    res.status(200).send('EVENT_RECEIVED');
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+
+          // Gets the body of the webhook event
+          let webhook_event = entry.messaging[0];
+          console.log(webhook_event);
+
+
+          // Get the sender PSID
+          let sender_psid = webhook_event.sender.id;
+          console.log('Sender PSID: ' + sender_psid);
+
+          // Check if the event is a message or postback and
+          // pass the event to the appropriate handler function
+          if (webhook_event.message) {
+              handleMessage(sender_psid, webhook_event.message);
+          } else if (webhook_event.postback) {
+              handlePostback(sender_psid, webhook_event.postback);
+          }
+
+      });
+
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    res.status(404).send('Loi roi')
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
   }
-
 };
 
 //handles message events
