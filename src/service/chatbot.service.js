f72d7ab71f6ec52fcb8315b01d92a01173c5c1c8
Duc Dat <datnho53@gmail.com>
2023-08-03 17:23:00 +0700

    get username when click to get started

--- src/service/chatbot.service.js
+++ src/service/chatbot.service.js
@@ -27,10 +27,32 @@ let callSendAPI = (sender_psid, response) => {
   });
 };
 
+let getUserName = (sender_psid) => {  
+    let username = "";
+    request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${FB_PAGE_TOKEN}`,
+        "qs": { "access_token": FB_PAGE_TOKEN },
+        "method": "GET"
+    }, (err, res, body) => {
+      console.log('body: ', body);
+        if (!err) {
+          let response = JSON.parse(res);
+
+          username = `${response.first_name} ${response.last_name}`;
+
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+
+    return username;
+}
+
 let handleGetStarted = (sender_psid) => {
   return new Promise( async (resolve, reject) => {
     try {
-      let response = { "text": "Xin chào bạn ABC đã đến với cửa hàng của chúng tôi, chúc bạn mua hàng vui vẻ, iuuuuuu... <3" };
+      let username = await getUserName(sender_psid);
+      let response = { "text": `Xin chào bạn ${username} đã đến với cửa hàng của chúng tôi, chúc bạn mua hàng vui vẻ, iuuuuuu... <3` };
       await callSendAPI(sender_psid, response);
       resolve('done');
     } catch (error) {
